7475bd109c944c903c79a6489ea8075215242193
clonnix <noahbrouwers24@gmail.com>
2026-01-18 15:25:10 +0100

    added dark theme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,14 @@
 const slider = document.getElementById('projectsContainer');
 const wrapper = document.querySelector('.projects-wrapper');
+const button = document.getElementById('theme-toggle');
+
+button.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    button.textContent =
+        document.body.classList.contains('dark')
+            ? '🌙'
+            : '☀️';
+});
 
 let scrollX = 0;
 
@@ -8,7 +17,7 @@ wrapper.addEventListener('wheel', e => {
     const wrapperWidth = wrapper.clientWidth;
     const maxScroll = containerWidth - wrapperWidth;
 
- 
+
     let newScrollX = scrollX + e.deltaY;
 
     newScrollX = Math.max(0, Math.min(newScrollX, maxScroll));
@@ -17,9 +26,9 @@ wrapper.addEventListener('wheel', e => {
     const canScrollHorizontally = newScrollX !== scrollX;
 
     if (canScrollHorizontally) {
-        e.preventDefault();      
+        e.preventDefault();
         scrollX = newScrollX;
         slider.style.transform = `translateX(${-scrollX}px)`;
     }
-    
+
 });
